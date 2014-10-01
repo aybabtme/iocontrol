c9fcc514355e51c4a5076ef5ff0b7af3c995ee80
Antoine Grondin <antoinegrondin@gmail.com>
2014-09-30 23:48:03 -0400

    initial README and doc.

---
+++ doc.go
@@ -0,0 +1,14 @@
+/*
+Package iocontrol offers `io.Writer` and `io.Reader` implementations
+that allow one to measure and throttle the rate at which data
+is transfered.
+*/
+package iocontrol
+
+// Orders of magnitude of data, in kibibyte (powers of 2, or multiples of 1024).
+// See https://en.wikipedia.org/wiki/Kibibyte.
+const (
+	KiB = 1 << 10
+	MiB = 1 << 20
+	GiB = 1 << 30
+)
